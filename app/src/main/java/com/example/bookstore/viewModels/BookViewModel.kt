851697655487c2d56dc795cc4827d113e24ce308
Chand <chand.mohd@blu-smart.com>
2024-05-15 09:08:39 +0530

    second  commit

--- app/src/main/java/com/example/bookstore/viewModels/BookViewModel.kt
+++ app/src/main/java/com/example/bookstore/viewModels/BookViewModel.kt
@@ -3,13 +3,14 @@ package com.example.bookstore.viewModels
 import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.bookstore.models.Book
 import com.example.bookstore.models.BooksResponse
 import com.example.bookstore.repository.BooksRepository
 
 
 class BookViewModel : ViewModel() {
 
-    private val _books:MutableLiveData<BooksResponse> = MutableLiveData()
+    private val _books:MutableLiveData<BooksResponse> = MutableLiveData() <List<Book>()
     val books = _books
 
 
@@ -17,7 +18,7 @@ class BookViewModel : ViewModel() {
     fun getBooks() {
         val repo = BooksRepository()
         val res = repo.getBookFromApi()
-       // Log.d("TAG", "getBooks vmn: ${res.books}")
+        Log.d("TAG", "getBooks vmn" + toString())
        // _books.value = res
 
     }
