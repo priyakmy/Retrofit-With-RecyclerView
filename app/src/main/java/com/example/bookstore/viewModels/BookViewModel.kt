8ef6c2a3fe26c63c43e13630eb72e10ec43fb026
Chand <chand.mohd@blu-smart.com>
2024-05-15 09:18:52 +0530

    commit

--- app/src/main/java/com/example/bookstore/viewModels/BookViewModel.kt
+++ app/src/main/java/com/example/bookstore/viewModels/BookViewModel.kt
@@ -1,17 +1,18 @@
 package com.example.bookstore.viewModels
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.bookstore.models.Book
-import com.example.bookstore.models.BooksResponse
 import com.example.bookstore.repository.BooksRepository
+import com.example.bookstore.retrofit.RetrofitInstance
 
 
 class BookViewModel : ViewModel() {
 
-    private val _books:MutableLiveData<BooksResponse> = MutableLiveData() <List<Book>()
-    val books = _books
+    private val _books = MutableLiveData() <List<Book>>()
+    val books: LiveData<List<Book>> = _books
 
 
 
@@ -20,6 +21,7 @@ class BookViewModel : ViewModel() {
         val res = repo.getBookFromApi()
         Log.d("TAG", "getBooks vmn" + toString())
        // _books.value = res
+        _books.value = RetrofitInstance.bookApi.getRandomBookList()
 
     }
 }
